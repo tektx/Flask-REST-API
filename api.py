674eb0f0dd9fceb750164ba8935e6f1a5144f678
tektx <tektx@users.noreply.github.com>
2016-06-12 00:55:35 -0500

    Add error handlers and rename methods and variables
    Add error handlers for 401 and 403, and include status code in the
    response.
    Fix method and variable names to be more intuitive.

--- api.py
+++ api.py
@@ -7,35 +7,38 @@ format. This data is then accessible from REST API endpoints.
 
 import csv
 import sys
-import flask
+from flask import Flask, jsonify, make_response, request, abort
 
 __author__ = "Travis Knight"
 __email__ = "Travisknight@gmail.com"
 __license__ = "BSD"
 
-app = flask.Flask(__name__)
-tasks = []
+app = Flask(__name__)
+db = []
 
 
-def get_businesses(csv_path):
+def create_db(csv_path):
     """Reads the CSV of businesses and processes them into dictionaries.
 
     Args:
         csv_path (str): The path to the CSV file.
 
     Returns:
-        bool: True if the processing was successful, False otherwise.
+        list[dict{str,str}]: The business info in JSON-formatted dictionaries.
 
     Raises:
         IOError: No file was found using the provided path.
     """
+    businesses = []
+
     try:
         with open(csv_path, 'r') as csv_file:
             csv_file.seek(0)
             reader = csv.DictReader(csv_file)
             for row in reader:
                 row['id'] = int(row['id'])
-                tasks.append(row)
+                businesses.append(row)
+        return businesses
 
     except IOError as ioe:
         print ioe
@@ -43,43 +46,66 @@ def get_businesses(csv_path):
 
 
 @app.route('/businesses', methods=['GET'])
-def get_tasks():
-    records = 3
-    # Pagination
-    if flask.request.json and 'page' in flask.request.json:
-        page = flask.request.json.get('page')
-        start = (page-1) * records
-        end = start + records
-        return flask.jsonify({'tasks': tasks[start:end]})
+def get_businesses():
+    entries = 3
+    if 'page' in request.args:
+        page = int(request.args['page'])
+        start = (page-1) * entries
+        end = start + entries
+        if len(db[start:end]) > 0:
+            return jsonify({'businesses': db[start:end], 'page': page})
+        else:
+            abort(404)
     else:
-        return flask.jsonify({'tasks': tasks[:records]})
+        return jsonify({'businesses': db[:entries]})
 
 
 @app.route('/businesses/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        flask.abort(404)
-    return flask.jsonify({'task': task[0]})
+def get_business(task_id):
+    business = [business for business in db if business['id'] == task_id]
+    if len(business) == 0:
+        abort(404)
+    return jsonify({'business': business[0]})
 
 
 @app.errorhandler(400)
 def bad_request(error):
-    return flask.make_response(flask.jsonify({'error': 'Bad request'}), 400)
+    return make_response(
+        jsonify({'status': 400,
+                 'error': 'The server cannot process the request.'}), 400)
+
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return make_response(
+        jsonify({'status': 401,
+                 'error': 'The authentication was not successful.'}), 401)
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return make_response(
+        jsonify({'status': 403,
+                 'error': 'The request is not authorized.'}), 403)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return flask.make_response(flask.jsonify({'error': 'Not found'}), 404)
+    return make_response(
+        jsonify({'status': 404,
+                 'error': 'The requested resource was not found.'}), 404)
 
 
 @app.errorhandler(405)
 def not_allowed(error):
-    return flask.make_response(flask.jsonify({'error': 'Not allowed'}), 405)
+    return make_response(
+        jsonify({'status': 405,
+                 'error': 'The request method is not supported.'}), 405)
 
 
 def main():
-    get_businesses('sample.csv')
+    global db
+    db = create_db('sample.csv')
     app.run(debug=True)
 
 
