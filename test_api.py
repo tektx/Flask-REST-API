7067975c5da1d5b3d4bb4f36dffa7f5f9cc5e512
tektx <tektx@users.noreply.github.com>
2016-07-20 22:57:23 -0500

    Fix 404 test
    Open the correct file for test data.

--- test_api.py
+++ test_api.py
@@ -1,5 +1,4 @@
-"""Tests the REST API application.
-"""
+"""Tests the REST API application."""
 
 import json
 import unittest
@@ -12,21 +11,16 @@ __license__ = "BSD"
 class APITestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db = api.create_db('sample.csv')
+        self.db = api.create_db('50k_businesses.csv')
         self.app = api.app.test_client()
 
     def tearDown(self):
         pass
 
     def test_404(self):
-        """Checks that 404 errors include their status code in the response.
-        """
+        """Checks that 404 errors include their status code in the response."""
         response = json.loads(self.app.get('/').data)
         self.assertEqual(response['status'], 404)
-
-    def test_pagination(self):
-        response = json.loads(self.app.get('/businesses').data)
-        print response
 
 
 if __name__ == '__main__':
